7265d8169f998145492b0aa878d6a9ff93b1d503
root <root@localhost.localdomain>
2018-11-04 05:45:42 -0500

    oh

--- unordered_map/Unordered_map.cpp
+++ unordered_map/Unordered_map.cpp
@@ -1,7 +1,5 @@
 #include "Unordered_map.hpp"
-//#define INIT_SIZE 21788233
 
-//#ifndef TEST_DEBUG
 #define INIT_SIZE 52371
 using size_type = Unordered_map::size_type;
 using key_type = Unordered_map::key_type;
@@ -9,7 +7,7 @@ using key_type = Unordered_map::key_type;
 
 size_type Unordered_map::Hash(const key_type &key)
 {
-	register unsigned int hash= 0;
+	register unsigned hash= 0;
 	for(int i = 0; i < key.size(); i++)
 	{
 		hash = key[i] + (hash << 6) + (hash << 16) - hash;
@@ -73,7 +71,7 @@ void Unordered_map::insert(const value_type& value)
 
 T& Unordered_map::at(const Key& key)
 {
-	unsigned int hash = Hash(key);
+	unsigned hash = Hash(key);
 	list<value_type> *hash_list = table_[hash];
 	for(list<value_type>::iterator it = hash_list->begin(); it != hash_list->end(); it++)
 	{
@@ -102,4 +100,3 @@ Unordered_map::~Unordered_map()
 {
 	delete[] table_;
 }
-//#endif
